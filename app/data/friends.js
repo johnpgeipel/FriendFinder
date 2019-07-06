60189010adb241875cd332a0b83a01f311be629b
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-07-06 15:34:05 -0500

    adjusted some of the survey questions and added 'seed' data

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+// an array that holds all of the friends data
+var friendsData = [
+	{
+		"name": "Sparkles",
+		"photo": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+		"scores": [
+			1,
+			4,
+			2,
+			5,
+			5,
+			3,
+			2,
+			4,
+			3,
+			2
+		]
+	},
+	{
+		"name": "Theodore",
+		"photo": "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+		"scores": [
+			1,
+			5,
+			4,
+			5,
+			1,
+			1,
+			4,
+			5,
+			5,
+			5
+		]
+	},
+	{
+		"name": "Dr. Katz",
+		"photo": "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+		"scores": [
+			3,
+			2,
+			2,
+			1,
+			2,
+			5,
+			5,
+			2,
+			1,
+			4
+		]
+	},
+	{
+		"name": "Iggy",
+		"photo": "https://images.pexels.com/photos/208984/pexels-photo-208984.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+		"scores": [
+			5,
+			5,
+			3,
+			2,
+			3,
+			2,
+			4,
+			5,
+			1,
+			1
+		]
+	},
+	{
+		"name": "Monkey",
+		"photo": "https://images.pexels.com/photos/2061057/pexels-photo-2061057.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+		"scores": [
+			4,
+			1,
+			5,
+			3,
+			5,
+			4,
+			5,
+			5,
+			5,
+			2
+		]
+	},
+	{
+		"name": "Derp",
+		"photo": "https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+		"scores": [
+			2,
+			1,
+			1,
+			3,
+			2,
+			4,
+			1,
+			1,
+			1,
+			1
+		]
+	}
+];
+
+module.exports = friendsData;
